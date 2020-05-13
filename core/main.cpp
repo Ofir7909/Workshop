dc61ce08c79695ec2736ed6c747a765dd3f0d9c2
Ofir Lachmi <ofir7909@walla.com>
2020-05-13 17:04:59 +0300

    Added ImGui::Dockspace

--- core/main.cpp
+++ core/main.cpp
@@ -4,6 +4,10 @@
 
 #include <GLFW/glfw3.h>
 
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
 int main()
 {
 	GLFWwindow* window;
@@ -13,7 +17,7 @@ int main()
 		return -1;
 
 	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	window = glfwCreateWindow(1280, 720, "Workshop", NULL, NULL);
 	if (!window) {
 		glfwTerminate();
 		return -1;
@@ -21,6 +25,7 @@ int main()
 
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
+	glfwSwapInterval(1); // Enable Vsync
 
 	// Init GLAD
 	if (!gladLoadGL()) {
@@ -28,11 +33,68 @@ int main()
 		exit(-1);
 	}
 
+	// Imgui setup
+	// IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+	(void)io;
+	io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+	// io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+	ImGui::StyleColorsDark();
+
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 460");
+
+	bool show_another_window = true;
+
 	/* Loop until the user closes the window */
 	while (!glfwWindowShouldClose(window)) {
 		/* Render here */
+
+		{ // IMGUI WINDOW
+			ImGui_ImplOpenGL3_NewFrame();
+			ImGui_ImplGlfw_NewFrame();
+			ImGui::NewFrame();
+
+			// Window with Docking Space
+
+			ImGuiViewport* viewport = ImGui::GetMainViewport();
+			ImGui::SetNextWindowPos(viewport->GetWorkPos());
+			ImGui::SetNextWindowSize(viewport->GetWorkSize());
+			ImGui::SetNextWindowViewport(viewport->ID);
+
+			ImGui::Begin("DockSpace Demo");
+
+			ImGuiID dockspace_id = ImGui::GetID("MyDockSpace");
+			ImGui::DockSpace(dockspace_id);
+			ImGui::End();
+
+			ImGui::ShowDemoWindow();
+		}
+
+		// 3. Show another simple window.
+		if (show_another_window) {
+			ImGui::Begin("Another Window",
+						 &show_another_window); // Pass a pointer to our bool variable (the window will have a closing
+												// button that will clear the bool when clicked)
+			ImGui::Text("Hello from another window!");
+			if (ImGui::Button("Close Me"))
+				show_another_window = false;
+			ImGui::End();
+		}
+		// finish imgui frame
+		ImGui::Render();
+		int display_w, display_h;
+		glfwGetFramebufferSize(window, &display_w, &display_h);
+		glViewport(0, 0, display_w, display_h);
+
+		// Clear the screen
 		glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Draw imgui to the buffer
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
@@ -41,6 +103,17 @@ int main()
 		glfwPollEvents();
 	}
 
+	//
+	// CLEAN UP
+	//
+
+	// clean imgui
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
+
+	// clean glfw
+	glfwDestroyWindow(window);
 	glfwTerminate();
 	return 0;
 }
