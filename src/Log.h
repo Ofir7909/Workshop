eb9706baf811e147a8d345edc1dfc8b35dc34ce0
Ofir Lachmi <ofir7909@walla.com>
2020-06-13 13:45:19 +0300

    Swaped glad with its source files from the web tool

--- src/Log.h
+++ src/Log.h
@@ -11,7 +11,7 @@ class Log
   public:
 	static void Init();
 
-	inline static std::shared_ptr<spdlog::logger>& const GetLogger() { return s_Logger; }
+	inline static std::shared_ptr<spdlog::logger>& GetLogger() { return s_Logger; }
 
   private:
 	static std::shared_ptr<spdlog::logger> s_Logger;
